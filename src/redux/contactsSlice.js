6804951e832361c7e13c2aaba0280f1dd7ef46b2
Gizemnur Avcı <gizemnuravci4@gmail.com>
2026-04-29 21:05:38 +0300

    fix: total reset of file paths and casing

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./contactsOps";
-import { selectNameFilter } from "./filtersSlice";
+import { selectNameFilter } from "./filtersSlice.js";
 
 const handlePending = (state) => {
   state.loading = true;
