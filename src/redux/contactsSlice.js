600053f0ff258dc6cf7bba65ff39576833652aff
Gizemnur Avcı <gizemnuravci4@gmail.com>
2026-04-29 21:11:40 +0300

    fix: resolve casing issues and fix import paths

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./contactsOps";
-import { selectNameFilter } from "./filtersSlice.js";
+import { selectNameFilter } from "../redux/filterSlice";
 
 const handlePending = (state) => {
   state.loading = true;
